3da3a589a18353d3295b84e9a4063388a1711490
AAcosta9600 <alejandro.acosta@revature.net>
2021-11-05 16:07:00 -0400

    added logging to all methods that make sense

--- src/main/java/com/revature/controllers/ErsReimbursementController.java
+++ src/main/java/com/revature/controllers/ErsReimbursementController.java
@@ -2,6 +2,9 @@ package com.revature.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.revature.models.ErsReimbursement;
 import com.revature.service.ErsReimbursementService;
 
@@ -11,74 +14,99 @@ import io.javalin.http.Handler;
 public class ErsReimbursementController implements Controller {
 
 	private ErsReimbursementService ersReimbursementService = new ErsReimbursementService();
-
+	public static Logger myLogger = LoggerFactory.getLogger("myLogger");
+	
 	public Handler getAllReimbursements = (ctx) -> {
+		
 		if (ctx.req.getSession(false) != null) {
+			myLogger.info("Attempt to get all Reimbs");
 			List<ErsReimbursement> list = ersReimbursementService.getAllReimbursements();
 
 			ctx.json(list);
 			ctx.status(200);
+			myLogger.info("Attempt Successful");
 		} else {
 			ctx.status(401);
+			myLogger.info("Attempt failed");
 		}
 	};
 
 	public Handler getReimbursementByStatus = (ctx) -> {
+		
 		if (ctx.req.getSession(false) != null) {
+			myLogger.info("Attempt to get Reimbs by status");
 			try {
 				String idString = ctx.pathParam("statusId");
 				List<ErsReimbursement> list = ersReimbursementService.getReimbursementByStatus(idString);
 				ctx.json(list);
 				ctx.status(200);
+				myLogger.info("Attempt Successful");
 			}
 			catch(NumberFormatException e){
 				e.printStackTrace();
 				ctx.status(406);
 			}
 		}
+		else {
+			ctx.status(401);
+		}
 	};
 	
 	public Handler getMyReimbursement = (ctx) -> {
 		if (ctx.req.getSession(false) != null) {
+			myLogger.info("Attempt to get Reimbs by user");
 			try {
 				String idString = ctx.pathParam("userId");
 				int userId = Integer.parseInt(idString);
 				List<ErsReimbursement> list = ersReimbursementService.getMyReimbursement(userId);
 				ctx.json(list);
 				ctx.status(200);
+				myLogger.info("Attempt Successful");
 			}
 			catch(NumberFormatException e){
 				e.printStackTrace();
 				ctx.status(406);
+				myLogger.info("Attempt Failed");
 			}
+		}
+		else {
+			ctx.status(401);
 		}
 	};
 	
 	public Handler getReimbursementById = (ctx) -> {
 		if(ctx.req.getSession(false) != null) {
 			try {
+				myLogger.info("Attempt to get Reimbs by id");
 				String idString = ctx.pathParam("reimbId");
 				int reimbId = Integer.parseInt(idString);
 				List<ErsReimbursement> ersReimbursement= ersReimbursementService.getReimbursementById(reimbId);
 				ctx.json(ersReimbursement);
 				ctx.status(200);
+				myLogger.info("Attempt Successful");
 			}
 			catch(NumberFormatException e){
 				e.printStackTrace();
 				ctx.status(406);
+				myLogger.info("Attempt Failed");
 			}
+		}
+		else {
+			ctx.status(401);
 		}
 	};
 	
 	public Handler addReimbursement = (ctx) -> {
 		if(ctx.req.getSession(false) != null) {
-
+			myLogger.info("Attempt to add Reimb");
 				ErsReimbursement reimbursement= ctx.bodyAsClass(ErsReimbursement.class);
 				if(ersReimbursementService.addReimbursement(reimbursement)) {
 					ctx.status(201);
+					myLogger.info("Attempt Successful");
 				}
 				else {
 					ctx.status(400);
+					myLogger.info("Attempt Failed");
 				}
 			
 		}
@@ -89,12 +117,15 @@ public class ErsReimbursementController implements Controller {
 	
 	public Handler updateReimbursement = (ctx) -> {
 		if(ctx.req.getSession(false) != null) {
+			myLogger.info("Attempt to update reimb");
 				ErsReimbursement reimbursement= ctx.bodyAsClass(ErsReimbursement.class);
 				if(ersReimbursementService.updateReimbursement(reimbursement)) {
 					ctx.status(200);
+					myLogger.info("Attempt Successful");
 				}
 				else {
 					ctx.status(400);
+					myLogger.info("Attempt Failed");
 				}
 			
 		}
--- src/main/java/com/revature/controllers/ErsUserController.java
+++ src/main/java/com/revature/controllers/ErsUserController.java
@@ -1,5 +1,8 @@
 package com.revature.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.revature.models.ErsUser;
 import com.revature.models.UserDTO;
 import com.revature.service.ErsUserService;
@@ -10,7 +13,7 @@ import io.javalin.http.Handler;
 public class ErsUserController implements Controller{
 
 	private ErsUserService ersUserService = new ErsUserService();
-	
+	public static Logger myLogger = LoggerFactory.getLogger("myLogger");
 	
 	public Handler getUser = (ctx) -> {
 		//if(ctx.req.getSession(false) != null) {
@@ -26,13 +29,15 @@ public class ErsUserController implements Controller{
 	
 	public Handler addUser = (ctx) -> {
 		//if(ctx.req.getSession(false) != null) {
-			
+		myLogger.info("Attempt to register");
 			ErsUser ersUser = ctx.bodyAsClass(ErsUser.class);
 			if(ersUserService.addUser(ersUser)) {
 				ctx.status(201);
+				myLogger.info("Attempt Successful");
 			}
 			else {
 				ctx.status(400);
+				myLogger.info("Attempt Failed");
 			}
 		//}
 		//else {
@@ -56,14 +61,17 @@ public class ErsUserController implements Controller{
 	};
 	
 	private Handler loginAttempt = (ctx) -> {
+		myLogger.info("Attempt login");
 		UserDTO userDTO = ctx.bodyAsClass(UserDTO.class);
 		
 		if(ErsUserService.login(userDTO)) {
 			ctx.req.getSession(); 
 			ctx.status(200);
+			myLogger.info("Attempt Successful");
 		}else {
 			ctx.req.getSession().invalidate();
 			ctx.status(401);
+			myLogger.info("Attempt Failed");
 		}
 	};
 	
